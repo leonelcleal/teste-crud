ed51ab8002015b257132cbdd9cd2a8c6c1da016f
leonelcleal <lealcleonel@gmail.com>
2025-09-11 21:43:42 -0300

    POO

---
+++ POO/Classes/Aluno.java
@@ -0,0 +1,10 @@
+Package Classes;    
+public class Aluno {
+
+    private String prontuario;
+    private String nome;
+
+    public void estudar(); {
+        System.out.println("estudando...");
+    }
+
