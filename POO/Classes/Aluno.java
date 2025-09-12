81a6d2d10d631e94c00bffad8fd2621f2c53f98c
leonelcleal <lealcleonel@gmail.com>
2025-09-11 22:15:50 -0300

    Altera POO

--- POO/Classes/Aluno.java
+++ POO/Classes/Aluno.java
@@ -1,10 +1,28 @@
-Package Classes;    
+package Classes;    
 public class Aluno {
 
     private String prontuario;
     private String nome;
 
-    public void estudar(); {
+    public Aluno(String nome, String prontuario) {
+        this.nome = nome;
+        this.prontuario = prontuario;
+    }
+
+    public void estudar() {
         System.out.println("estudando...");
     }
 
+    public String getProntuario() {
+        return prontuario;
+    }
+    public void setProntuario(String prontuario) {
+        this.prontuario = prontuario;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
